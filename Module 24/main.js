a24d36c46e0e4b6aea6ff2d6df779acfe90d381e
Leon7065 <lonilee29@gmail.com>
2024-11-01 10:15:24 +0100

    functions

---
+++ Module 24/main.js
@@ -0,0 +1,41 @@
+// function showMessage(){
+//     alert("This message is inside a function");
+// }
+
+// showMessage();
+
+// function sum (x,y){
+//     return x+y;
+// }
+
+// document.write(sum(20, 5));
+
+// var arrowfunction= () => alert("Hello world");
+
+// arrowfunction();
+
+var arrowfunction = name => alert(`hello ${name}`);
+
+arrowfunction("Leon Berisha");
+
+function namE(){
+    var localVar = "Leon";
+    alert(localVar);
+}
+
+namE();
+
+//alert(localVar);
+
+
+function x(minutes){
+    return minutes*60;
+}
+
+x();
+document.write(x(60));
+
+
+
+
+
