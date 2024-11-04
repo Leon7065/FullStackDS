b43b2acd70c35c60f66db9f0091d32f7bb5add33
Leon7065 <lonilee29@gmail.com>
2024-11-04 12:10:27 +0100

    conditionals intro

---
+++ Lesson 27/main.js
@@ -0,0 +1,30 @@
+var x = 10;
+var y = 5;
+
+if (x>y) {
+    console.log("This is true");
+}else{
+    console.log("this is wrong");
+}
+
+
+if (x>y) {
+    console.log("This is true");
+}else{
+    console.log("this is wrong");
+}
+
+if (x==7) {
+    console.log("10 equals 7");
+}else{
+    console.log("10 does not equal 7");
+}
+
+
+
+
+
+
+
+
+
