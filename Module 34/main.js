d7ac7c9593024f687f437fe8d7476e4b26004ff3
Leon7065 <lonilee29@gmail.com>
2024-11-15 10:48:39 +0100

    accordian,tabs

---
+++ Module 34/main.js
@@ -0,0 +1,43 @@
+//jquery
+//$('myElement').hide();
+//manipulate with HTML 
+// $('#myElement').text("New Text");
+//$('.btn').click(function(){
+//    console.log($('#myElement').text());
+//    $('#myElement').text('Testing text');
+//    $('#myElement').append(' <br> Extra text');
+//});
+
+
+
+// $('#btn3').click(function(){
+//     $('.hidden').show('slow');
+// });
+
+// $('#btn4').click(function(){
+//     $('.hidden').hide();
+// });
+
+
+// $('#square').click(function(){
+//     $('#square').animate({
+//         'width':'200px',
+//         'height':'200px',
+//         // 'border-radius':'100px',
+//     });
+// });
+
+$('#sort').sortable();
+
+$(function(){
+    $('#accordion').accordion();
+});
+
+
+$(function(){
+    $('#tabs').tabs();
+});
+
+
+
+
