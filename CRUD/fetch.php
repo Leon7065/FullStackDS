b70b3a112830c8ead2987568037e826a8fd3b350
LeonBUBT <lonilee29@gmail.com>
2024-12-16 09:21:51 +0100

    .

--- CRUD/fetch.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('config.php');
-    $sql = "SELECT * from users";
-    $getUsers = $connect->prepare($sql);
-    $getUsers->execute();
-    $users = $getUsers ->fetchAll();
-?>
